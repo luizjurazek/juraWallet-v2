7ee73afe86027b55825471b28e15c8f2b50e3928
Luiz Jurazek <luizjurazek@gmail.com>
2024-02-15 19:15:22 -0300

    feat: create Category model using sequelize

--- models/categoryModel.js
+++ models/categoryModel.js
@@ -1,24 +1,29 @@
-const { sequelize } = require('../config/connection')
-const { DataTypes } = require('sequelize')
+const {
+	sequelize
+} = require('../config/connection')
+const {
+	DataTypes
+} = require('sequelize')
 
-const Category = {
-    id_category: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncremente: true
-    },
-    name_category: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    id_user: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
-            model: 'users',
-            key: 'id_user'
-        }
-    }
-}
-
+const Category = sequelize.define('Category', {
+	id_category: {
+		type: DataTypes.INTEGER,
+		primaryKey: true,
+		autoIncremente: true
+	},
+	name_category: {
+		type: DataTypes.STRING,
+		allowNull: false,
+	},
+	id_user: {
+		type: DataTypes.INTEGER,
+		allowNull: false,
+		references: {
+			model: 'users',
+			key: 'id_user'
+		}
+	}
+}, {
+  timestamps: false, // Isso habilita a criação automática dos campos createdAt e updatedAt
+})
 module.exports = Category;
