8e5dd24b8b28238d22af2633a71465520438e081
Luiz Jurazek <luizjurazek@gmail.com>
2024-02-15 19:35:02 -0300

    feat: create method for delete all categories

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,4 +1,6 @@
-const { response } = require('express')
+const {
+  response
+} = require('express')
 const Category = require('../models/categoryModel')
 const verifyAlreadyExistsItemInBD = require('../utils/verifyAlreadyExistsItemInBD')
 
@@ -101,6 +103,12 @@ const categoryController = {
     const id_user = req.userId
     const id_category = req.params.id
 
+
+    // Verificar se existem transações associadas à categoria
+    // const checkTransactionsQuery = 'SELECT COUNT(*) as count FROM transaction WHERE id_user = ?';
+    // const checkResult = await connection.promise().query(checkTransactionsQuery, id_user);
+    // const transactionCount = checkResult[0][0].count;
+
     try {
       const categoryDeleted = await Category.destroy({
         where: {
@@ -116,21 +124,21 @@ const categoryController = {
           error: false,
           message: "Categoria deletada com sucesso."
         }
-  
+
         return res.status(200).json(response)
       } else if (categoryDeleted == 0) {
         const response = {
           error: true,
           message: "Categoria não encontrada."
         }
-  
+
         return res.status(404).json(response)
       } else {
         const response = {
           error: true,
           message: "Houve um erro ao deletar a categoria."
         }
-  
+
         return res.status(400).json(response)
       }
     } catch (error) {
@@ -145,19 +153,50 @@ const categoryController = {
   deleteAllCategories: async (req, res) => {
     const id_user = req.userId
 
+    // Verificar se existem transações associadas à categoria
+    // const checkTransactionsQuery = 'SELECT COUNT(*) as count FROM transaction WHERE id_user = ?';
+    // const checkResult = await connection.promise().query(checkTransactionsQuery, id_user);
+    // const transactionCount = checkResult[0][0].count;
+
     try {
-      const deleteCategories = await categoryModel.deleteAllCategories(id_user)
-      if (deleteCategories.error == false) {
-        res.status(201).json(deleteCategories)
-      } else if (deleteCategories.error == true) {
-        res.status(400).json(deleteCategories)
-      } else {
-        res.status(500).json({
+      const deletedCategories = await Category.destroy({
+        where: {
+          id_user: id_user
+        }
+      })
+
+      console.log(deletedCategories)
+
+      if(deletedCategories != 0){
+        const response = {
+          error: false,
+          message: `Foram deletadas ${deletedCategories} categorias`
+        }
+
+        res.status(200).json(response)
+      } if (deletedCategories === 0){
+        const response = {
           error: true,
-          message: "Houve um erro no servidor."
-        })
-      }
+          message: `Não foram encontradas categorias.`
+        }
+
+        res.status(404).json(response)
+      } else {
+        const response = {
+          error: true,
+          message: `Houve um erro ao deletar as categorias.`
+        }
+
+        res.status(400).json(response)
+      } 
+
     } catch (error) {
+      res.status(500).json({
+        error: true,
+        message: "Houve um erro no servidor."
+      })
+
+
       throw error
     }
   },
