04649c89fd9e534b813d832a2f4883c588aa9a74
Luiz Jurazek <luizjurazek@gmail.com>
2024-03-30 10:09:20 -0300

    feat: create endpoint to delete a type of transaction

--- controllers/typeOfTransactionController.js
+++ controllers/typeOfTransactionController.js
@@ -1,15 +1,17 @@
-const typeOfTransactionModel = require('../models/typeoftransactionsModel')
+const TypeOfTransactionModel = require('../models/typeoftransactionsModel')
 
 const typeOfTransactionController = {
   createTypeOfTransaction: async (req, res, next) => {
-    let nameOfTypeOfTransaction = req.params.name
+    // #swagger.tags = ['Type of transaction']
+    // #swagger.description = 'Endpoint para criar um novo tipo de transação.'
+    let name_of_type_of_transaction = req.params.name
     let id_user = req.userId
 
     try {
 
-      const typeOfTransaction = await typeOfTransactionModel.create({
+      const typeOfTransaction = await TypeOfTransactionModel.create({
         id_user,
-        name_typeOfTransaction: nameOfTypeOfTransaction
+        name_typeOfTransaction: name_of_type_of_transaction.trim()
       })
 
       if(!typeOfTransaction){
@@ -21,12 +23,43 @@ const typeOfTransactionController = {
       return res.status(200).json({
         error: false,
         message: "Tipo de transação criada com sucesso.",
-        type_of_transaction: nameOfTypeOfTransaction
+        type_of_transaction: name_of_type_of_transaction
       })
     } catch (error){
       next(error)
     }
+  },
+  deleteTypeOfTransaction: async (req, res, next) => {
+    // #swagger.tags = ['Type of transaction']
+    // #swagger.description = 'Endpoint para deletar um tipo de transação.'
+    let id_type_of_transaction = req.params.id_type_of_transaction
+    let id_user = req.userId
+
+    try {
+      const existItem = await TypeOfTransactionModel.findAll({
+        where: {
+          id_user,
+          id_typeOfTransaction: id_type_of_transaction
+        }
+      })
+      
+      if(existItem.length === 0){
+        const error = new Error('Tipo de transação não encontrada.')
+        error.statusCode = 400
+        throw error
+      }
+
+      return res.status(200).json({
+        error: false,
+        message: "Tipo de transação deletada com sucesso!",
+        type_of_transaction: existItem
+      })
+
+    } catch (error){
+      next(error)
+    }
   }
+
 }
 
 module.exports = typeOfTransactionController
--- routes/routesTypeOfTransaction.js
+++ routes/routesTypeOfTransaction.js
@@ -4,5 +4,6 @@ const router = express.Router()
 const typeOfTransactionController = require('../controllers/typeOfTransactionController')
 
 router.post('/create-type-of-transaction/:name', typeOfTransactionController.createTypeOfTransaction)
+router.delete('/delete-type-of-transaction/:id_type_of_transaction', typeOfTransactionController.deleteTypeOfTransaction)
 
 module.exports = router
